2177a0fa55ccba503339097b205479f1f23d5bec
lyohas88b1m16 <63349974+lyohas88b1m16@users.noreply.github.com>
2020-04-12 10:20:04 +0700

    splitted answer on get and post methods

--- app/server/router.js
+++ app/server/router.js
@@ -7,35 +7,14 @@ const Router = {
     notFound: './app/public/404.html'
   },
   listen: async function (request) {
-    let parsedURL = Url.parse(request.url, true)
-    let path = parsedURL.pathname.replace(/^\/+|\/+$/g, '')
-    let method = request.method.toUpperCase()
-    let query = JSON.stringify(parsedURL.query)
-
-    console.log('method', method)
-    console.log('path', path)
-    console.log('query', query)
+    const parsedURL = Url.parse(request.url, true)
+    const path = parsedURL.pathname.replace(/^\/+|\/+$/g, '')
+    const method = request.method.toLowerCase()
+    const query = JSON.stringify(parsedURL.query)
 
     let answer = {}
-
     try {
-      let content
-      switch (path) {
-        case '': {
-          console.log('index')
-          content = await fsPromise.readFile(this.places.index)
-          break
-        }
-        default: {
-          content = await fsPromise.readFile(this.places.notFound)
-        }
-      }
-
-      answer = {
-        status: 200,
-        type: { 'Content-Type': 'text/html' },
-        content,
-      }
+      answer = await this[method](path, query)
     } catch (e) {
       console.log(`Exception: ${e}`)
       answer = {
@@ -46,7 +25,40 @@ const Router = {
     }
 
     return answer
-  }
+  },
+  get: async function(path, query) {
+    console.log('get', path, query)
+    
+    let answer = {}
+
+    switch (path) {
+      case '': {
+        console.log('index')
+
+        answer.content = await fsPromise.readFile(this.places.index)
+        answer.status = 200
+        break
+      }
+      default: {
+        answer.content = await fsPromise.readFile(this.places.notFound)
+        answer.status = 404
+      }
+    }
+    
+    answer.type = { 'Content-Type': 'text/html' }
+
+    return answer
+  },
+  post: async function(path, query) {
+    console.log('post', path, query)
+    let answer = {
+      status: 404,
+      type: { 'Content-Type': 'text/html' },
+      content: '404 Not Found',
+    }
+
+    return answer
+  },
 }
 
 module.exports = Router
